61f94901e27d6353949cf2b15f9f50b6b9dd7baf
mdahmen <manuel.dahmen@gmail.com>
2014-11-28 23:30:03 +0100

    Card+

--- src/info/emptycanvas/apps/darkfortress/Card.java
+++ src/info/emptycanvas/apps/darkfortress/Card.java
@@ -10,6 +10,7 @@ Global license :
 
 package info.emptycanvas.apps.darkfortress;
 
+import info.emptycanvas.library.object.Point3D;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 
@@ -20,9 +21,28 @@ import java.awt.image.BufferedImage;
 public abstract class Card {
     public final int CARD_TYPE_COLORS = 0;
     public final int CARD_TYPE_POLYGONS = 1;
+    protected int width;
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    protected int height;
     
-    public abstract GamePosition getGamePosition();
-    public abstract void setGamePosition(GamePosition coordonnees);
+    public abstract Point3D getGamePosition();
+    public abstract void setGamePosition(Point3D coordonnees);
     public abstract BufferedImage genererImage();
     public abstract void initCard(int width, int height, Color [][] objet);
+    
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
 }
--- src/info/emptycanvas/apps/darkfortress/SimpleCard.java
+++ src/info/emptycanvas/apps/darkfortress/SimpleCard.java
@@ -10,8 +10,16 @@ Global license :
 
 package info.emptycanvas.apps.darkfortress;
 
+import info.emptycanvas.library.object.Camera;
+import info.emptycanvas.library.object.Point3D;
+import info.emptycanvas.library.object.Polygone;
+import info.emptycanvas.library.object.Scene;
+import info.emptycanvas.library.object.ZBuffer;
+import info.emptycanvas.library.object.ZBufferFactory;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  *
@@ -19,32 +27,51 @@ import java.awt.image.BufferedImage;
  */
 public class SimpleCard extends Card
 {
-    private int width;
-    private int height;
-    private GamePosition gamePosition;
+    private Point3D gamePosition;
+    private final Camera camera;
+    private final ArrayList<Polygone> polygones;
+    private ZBuffer z;
+
+    
+    public ArrayList<Polygone> getPolygones() {
+        return polygones;
+    }
     
     public SimpleCard()
     {
-        
+        polygones = new ArrayList<Polygone>();
+        camera = new Camera(new Point3D(0.5,1,0.5), new Point3D(0.5,0,0.5));
     }
     
-    
+    public Camera camera()
+    {
+        return camera;
+    }
+
+    public void update()
+    {
+        z = ZBufferFactory.instance(width, height);
+        z.scene(new Scene());
+        Iterator<Polygone> itP;
+        itP = polygones.iterator();
+        while(itP.hasNext())
+            z.scene().add(itP.next());
+        z.scene().add(gamePosition);
+    }
     
     @Override
-    public GamePosition getGamePosition() {
+    public Point3D getGamePosition() {
         return gamePosition;
     }
 
     @Override
-    public void setGamePosition(GamePosition coordonnees) {
+    public void setGamePosition(Point3D coordonnees) {
         this.gamePosition = coordonnees;
     }
-
+    
     @Override
     public BufferedImage genererImage() {
-        BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        bi.setRGB((int)(gamePosition.positionCard().getX())*width, (int)(gamePosition.positionCard().getY()*height), Color.YELLOW.getRGB());
-        return bi;
+        return z.image();
     }
 
     @Override
